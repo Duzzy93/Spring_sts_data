7b54710c227df5cf7eee3f79b0540a384b5984a0
yoonsuk914 <yoonsuk914@naver.com>
2025-06-05 15:01:49 +0900

    mybatis를 이용한 회원가입페이지 구현 및 데이터베이스 연동

--- ex01/src/main/java/com/company/repository/MemberRepository.java
+++ ex01/src/main/java/com/company/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.company.repository;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.company.dto.MemberDTO;
@@ -9,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
+	
+	private final SqlSessionTemplate sql;
+	
 	public int save(MemberDTO memberDTO) {
 		System.out.println("memberDTO = " + memberDTO);
-		return 0;
+		return sql.insert("Member.save", memberDTO);
 	}
 
 }
